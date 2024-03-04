d7e4812f385be2d76e2af5d7f8d86d9e95b46c96
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-03-04 17:58:39 +0100

    Working on the Authentication installations

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {  Text } from 'react-native';
 
 import { useFonts } from 'expo-font';
 
