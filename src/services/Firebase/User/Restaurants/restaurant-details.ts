2d8e0ca8579e7613d36d11a08a345addeed57fd1
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-05-04 20:36:22 +0100

    Added a comment to the firebase function get

--- src/services/Firebase/User/Restaurants/restaurant-details.ts
+++ src/services/Firebase/User/Restaurants/restaurant-details.ts
@@ -3,6 +3,7 @@
 import { database } from '../../../../firebaseConfig';
 import { ref, onValue, set, push, limitToFirst, query } from 'firebase/database';
 
+// Get specific restaurant details
 const getRestaurantInfo = (uid: string) => {
     onValue(
         ref(database, `Restaurant/${uid}`),
