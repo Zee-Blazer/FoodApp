0c715b6c07f3c92e7601a0924038096c7acd4158
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-05-13 13:48:42 +0100

    About to start working on the display sectiomn

--- src/screens/Users/only-restaurant.screen.tsx
+++ src/screens/Users/only-restaurant.screen.tsx
@@ -21,7 +21,7 @@ export const OnlyRestaurantScreen = () => {
 
                 <RestaurantDetailContainerComponent 
                     show={ true }
-                />
+                /> 
 
             </View>
 
