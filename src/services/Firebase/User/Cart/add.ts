73621f74c18479dfeeb8cf5167051c9ec4c57116
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-05-22 18:34:34 +0100

    Added a function to help check for items before adding

--- src/services/Firebase/User/Cart/add.ts
+++ src/services/Firebase/User/Cart/add.ts
@@ -3,27 +3,38 @@
 import { database } from '../../../../firebaseConfig';
 import { ref, onValue, push } from 'firebase/database';
 
+const addData = (uid: string, path: string) => {
+    push(
+        ref(database, `Cart/${uid}`),
+        {
+            uid, path, num: 1
+        }
+    )
+}
+
 const addToCart = (uid: string, path: string) => {
 
     onValue(
         ref(database, `Cart/${uid}`),
         ( snapshot ) => {
             if(snapshot.exists()){
-                const data = Object.values(snapshot.val());
-                const checker = data.find( (e: any) => e.path === path );
-                console.log(checker)
+                // Created the data into an array form
+                const data = Object.values(snapshot.val()); 
+                
+                // Check if the new item already exists
+                const checker = data.filter( (e:any) => e.path == path ); 
+
+                // If the new item doesn't exist, then add to cart
+                if(checker.length == 0){
+                    addData(uid, path);
+                }
             }
             else{
-                console.log("No snapshot")
+                addData(uid, path);
             }
         }
     );
-    // push(
-    //     ref(database, `Cart/${uid}`),
-    //     {
-    //         uid, path, num: 1
-    //     }
-    // )
+    
 }
 
 export {
