86f511f9d8d990e610846c3b74d84dbca19cb339
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-05-22 18:23:28 +0100

    Creating a function that would automatically check if the item is already in the cart

--- src/services/Firebase/User/Cart/add.ts
+++ src/services/Firebase/User/Cart/add.ts
@@ -4,12 +4,26 @@ import { database } from '../../../../firebaseConfig';
 import { ref, onValue, push } from 'firebase/database';
 
 const addToCart = (uid: string, path: string) => {
-    push(
+
+    onValue(
         ref(database, `Cart/${uid}`),
-        {
-            uid, path, num: 0
+        ( snapshot ) => {
+            if(snapshot.exists()){
+                const data = Object.values(snapshot.val());
+                const checker = data.find( (e: any) => e.path === path );
+                console.log(checker)
+            }
+            else{
+                console.log("No snapshot")
+            }
         }
-    )
+    );
+    // push(
+    //     ref(database, `Cart/${uid}`),
+    //     {
+    //         uid, path, num: 1
+    //     }
+    // )
 }
 
 export {
