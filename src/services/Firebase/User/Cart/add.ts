940b76b1f8cceab0037565bbeee6bf7fc3697a4e
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-05-22 13:37:54 +0100

    About to create function that would help handle the cart creation to the database

---
+++ src/services/Firebase/User/Cart/add.ts
@@ -0,0 +1,10 @@
+
+// Firebase Database
+import { database } from '../../../../firebaseConfig';
+import { ref, onValue, push } from 'firebase/database';
+
+const addToCart = () => {}
+
+export {
+    addToCart
+}
