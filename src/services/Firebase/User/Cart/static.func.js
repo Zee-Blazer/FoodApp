baa1567dd66e1f71829160a5d3dae20b0d62ecb4
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-05-28 17:20:29 +0100

    Properly getting the right data

--- src/services/Firebase/User/Cart/static.func.js
+++ src/services/Firebase/User/Cart/static.func.js
@@ -6,8 +6,15 @@ import { ref, onValue, push, update, remove } from 'firebase/database';
 const getStaticData = (data) => {
     let count = 0;
 
-    data.forEach( obj => console.log(obj) )
-    console.log("Working fine", data);
+    data.forEach( obj => {
+        onValue(
+            ref(database, `Category/${obj.path}`),
+            ( snapshot ) => {
+                console.log(snapshot.val().item_price * obj.num);
+            }
+        )
+    } )
+    // console.log("Working fine", data);
 }
 
 export {
